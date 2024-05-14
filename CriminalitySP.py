d30d8f14b47d17aebfc7344dfcf7e634d259b48e
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-14 12:06:40 -0300

    CriminalitySP.py

--- CriminalitySP.py
+++ CriminalitySP.py
@@ -21,7 +21,6 @@ st.sidebar.markdown('''
 [![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
 [![License]( https://img.shields.io/github/license/kauefs/StreamLit?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
             ''')
-st.sidebar.text( '13 October 2023')
 st.sidebar.divider(               )
 st.sidebar.title('DashBoard')
 st.sidebar.bar_chart(ocorrencias, height=200, color='#00BFFF')
